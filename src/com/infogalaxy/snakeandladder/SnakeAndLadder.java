1046ada3d7190cce58a30950d2cc56eb8625969c
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-03-11 10:35:51 +0530

    [ADDED]:Nikhil-Started With Single Player Position

---
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.infogalaxy.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) 
+	{
+		int pos = 0;
+		System.out.println(" The Position of a Player is : " +pos);
+	}
+
+}
